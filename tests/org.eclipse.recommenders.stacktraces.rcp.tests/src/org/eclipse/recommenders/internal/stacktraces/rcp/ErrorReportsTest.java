eclipse/recommenders

[stacktraces] Bug 446224: Status in CoreException handled in ErrorReports
Change-Id: I83db5831c13af847e071e45940e061d1dfc8f217
Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/ErrorReportsTest.java
@@ -10,12 +10,14 @@
  */
 package org.eclipse.recommenders.internal.stacktraces.rcp;
 
+import static org.eclipse.recommenders.internal.stacktraces.rcp.model.ErrorReports.newErrorReport;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
@@ -53,7 +55,7 @@ public class ErrorReportsTest {
                 exception);
 
         settings = ModelFactory.eINSTANCE.createSettings();
-        return ErrorReports.newErrorReport(status, settings);
+        return newErrorReport(status, settings);
     }
 
     private static Throwable createThrowable(String className) {
@@ -166,6 +168,24 @@ public class ErrorReportsTest {
         org.eclipse.recommenders.internal.stacktraces.rcp.model.Status multi = ErrorReports.newStatus(s2, settings);
 
         Assert.assertNotEquals(normal.getFingerprint(), multi.getFingerprint());
+    }
+
+    @Test
+    public void testCoreExceptionHandling() {
+        IStatus firstExceptionStatus = new Status(IStatus.ERROR, "the.first.plugin", "first message");
+        java.lang.Throwable firstException = new CoreException(firstExceptionStatus);
+        IStatus secondStatus = new Status(IStatus.WARNING, "some.other.plugin", "any other message", firstException);
+        java.lang.Throwable secondException = new CoreException(secondStatus);
+        IStatus event = new Status(IStatus.ERROR, "org.eclipse.recommenders.stacktraces", "someErrorMessage",
+                secondException);
+        settings = ModelFactory.eINSTANCE.createSettings();
+
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status status = ErrorReports.newStatus(event, settings);
+
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status firstChild = status.getChildren().get(0);
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status secondChild = firstChild.getChildren().get(0);
+        assertThat(firstChild.getPluginId(), is("some.other.plugin"));
+        assertThat(secondChild.getPluginId(), is("the.first.plugin"));
     }
 
     private static RuntimeException newRuntimeException(String message) {

kolipakakondal/error_reporting

tests/org.eclipse.recommenders.stacktraces.rcp.tests/src/org/eclipse/recommenders/internal/stacktraces/rcp/ErrorReportsTest.java
@@ -10,12 +10,14 @@
  */
 package org.eclipse.recommenders.internal.stacktraces.rcp;
 
+import static org.eclipse.recommenders.internal.stacktraces.rcp.model.ErrorReports.newErrorReport;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
@@ -53,7 +55,7 @@ public class ErrorReportsTest {
                 exception);
 
         settings = ModelFactory.eINSTANCE.createSettings();
-        return ErrorReports.newErrorReport(status, settings);
+        return newErrorReport(status, settings);
     }
 
     private static Throwable createThrowable(String className) {
@@ -166,6 +168,24 @@ public class ErrorReportsTest {
         org.eclipse.recommenders.internal.stacktraces.rcp.model.Status multi = ErrorReports.newStatus(s2, settings);
 
         Assert.assertNotEquals(normal.getFingerprint(), multi.getFingerprint());
+    }
+
+    @Test
+    public void testCoreExceptionHandling() {
+        IStatus causingStatus = new Status(IStatus.ERROR, "the.causing.plugin", "first message");
+        java.lang.Throwable causingException = new CoreException(causingStatus);
+        IStatus causedStatus = new Status(IStatus.WARNING, "some.calling.plugin", "any other message", causingException);
+        java.lang.Throwable rootException = new CoreException(causedStatus);
+        IStatus rootEvent = new Status(IStatus.ERROR, "org.eclipse.recommenders.stacktraces", "someErrorMessage",
+                rootException);
+        settings = ModelFactory.eINSTANCE.createSettings();
+
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status rootStatus = ErrorReports.newStatus(rootEvent, settings);
+
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status child = rootStatus.getChildren().get(0);
+        org.eclipse.recommenders.internal.stacktraces.rcp.model.Status leaf = child.getChildren().get(0);
+        assertThat(child.getPluginId(), is("some.calling.plugin"));
+        assertThat(leaf.getPluginId(), is("the.causing.plugin"));
     }
 
     private static RuntimeException newRuntimeException(String message) {