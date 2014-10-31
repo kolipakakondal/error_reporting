eclipse/recommenders

[stacktraces] Minor dialog message improvements
Change-Id: I0ec75ce51cfd60f916185fedf448aadddde8684d

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/Messages.java
@@ -48,7 +48,7 @@ public class Messages extends NLS {
     public static String THANKYOUDIALOG_MARKED_UNKNOWN;
     public static String THANKYOUDIALOG_MATCHED_NEED_FURTHER_INFORMATION;
     public static String THANKYOUDIALOG_MATCHED_PLEASE_ADD_TO_CC;
-    public static String THANKYOUDIALOG_NO_FURTHER_INFORMATIONS;
+    public static String THANKYOUDIALOG_NO_FURTHER_INFORMATION;
     public static String THANKYOUDIALOG_NOT_ABLE_TO_REPRODUCE_PLEASE_VISIT;
     public static String THANKYOUDIALOG_PLEASE_NOTE_ADDITIONAL_PERMISSIONS;
     public static String THANKYOUDIALOG_RECEIVED_AND_TRACKED;
@@ -56,7 +56,7 @@ public class Messages extends NLS {
     public static String THANKYOUDIALOG_THANK_YOU;
     public static String THANKYOUDIALOG_THANK_YOU_FOR_HELP;
     public static String THANKYOUDIALOG_TRACKED_PLEASE_ADD_TO_CC;
-    public static String THANKYOUDIALOG_ADDITIONAL_INFORMATIONS;
+    public static String THANKYOUDIALOG_ADDITIONAL_INFORMATION;
     public static String TOOLTIP_ANONYMIZE_STACKTRACES;
     public static String TOOLTIP_CLEAR_MESSAGES;
     public static String TOOLTIP_SKIP_SIMILAR;

kolipakakondal/error_reporting

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/Messages.java
@@ -39,24 +39,24 @@ public class Messages extends NLS {
     public static String PREVIEWPAGE_LABEL_COMMENT;
     public static String PREVIEWPAGE_LABEL_MESSAGE;
     public static String PREVIEWPAGE_TITLE;
-    public static String THANKYOUDIALOG_FURTHER_INFORMATION;
+    public static String THANKYOUDIALOG_COMMITTER_MESSAGE;
     public static String THANKYOUDIALOG_INVALID_SERVER_RESPONSE;
     public static String THANKYOUDIALOG_MARKED_DUPLICATE;
     public static String THANKYOUDIALOG_MARKED_FIXED;
     public static String THANKYOUDIALOG_MARKED_MOVED;
     public static String THANKYOUDIALOG_MARKED_NORMAL;
     public static String THANKYOUDIALOG_MARKED_UNKNOWN;
-    public static String THANKYOUDIALOG_MATCHED_NEED_FURTHER_INFORMATION;
-    public static String THANKYOUDIALOG_MATCHED_PLEASE_ADD_TO_CC;
-    public static String THANKYOUDIALOG_NO_FURTHER_INFORMATIONS;
-    public static String THANKYOUDIALOG_NOT_ABLE_TO_REPRODUCE_PLEASE_VISIT;
+    public static String THANKYOUDIALOG_COMMITTER_NEEDINFO;
+    public static String THANKYOUDIALOG_MATCHED_EXISTING_BUG;
+    public static String THANKYOUDIALOG_COMMITTER_MESSAGE_EMPTY;
+    public static String THANKYOUDIALOG_MARKED_WORKSFORME;
     public static String THANKYOUDIALOG_PLEASE_NOTE_ADDITIONAL_PERMISSIONS;
     public static String THANKYOUDIALOG_RECEIVED_AND_TRACKED;
     public static String THANKYOUDIALOG_RECEIVED_UNKNOWN_SERVER_RESPONSE;
     public static String THANKYOUDIALOG_THANK_YOU;
     public static String THANKYOUDIALOG_THANK_YOU_FOR_HELP;
-    public static String THANKYOUDIALOG_TRACKED_PLEASE_ADD_TO_CC;
-    public static String THANKYOUDIALOG_ADDITIONAL_INFORMATIONS;
+    public static String THANKYOUDIALOG_NEW;
+    public static String THANKYOUDIALOG_ADDITIONAL_INFORMATION;
     public static String TOOLTIP_ANONYMIZE_STACKTRACES;
     public static String TOOLTIP_CLEAR_MESSAGES;
     public static String TOOLTIP_SKIP_SIMILAR;