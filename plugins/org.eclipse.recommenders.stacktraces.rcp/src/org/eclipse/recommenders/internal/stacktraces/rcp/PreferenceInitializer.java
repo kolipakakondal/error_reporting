eclipse/recommenders

[stacktraces] Moved values for configuration to separate class
Change-Id: Iacb06e23cdf8d5a6ba750c72d9944427f4059853
Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/PreferenceInitializer.java
@@ -40,9 +40,8 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
         s.put(PROP_NAME, "");
         s.put(PROP_EMAIL, "");
         s.putBoolean(PROP_SKIP_SIMILAR_ERRORS, true);
-        s.put(PROP_WHITELISTED_PLUGINS, "org.eclipse.;org.apache.log4j.;com.codetrails.;");
-        s.put(PROP_WHITELISTED_PACKAGES,
-                "org.eclipse.;;;org.apache.;java.;javax.;javafx.;sun.;com.sun.;com.codetrails.;org.osgi.;com.google.;ch.qos.;org.slf4j.;");
+        s.put(PROP_WHITELISTED_PLUGINS, Configuration.WHITELISTED_PLUGINS);
+        s.put(PROP_WHITELISTED_PACKAGES, Configuration.WHITELISTED_PACKAGES);
         s.put(PROP_SEND_ACTION, SendAction.ASK.name());
         s.putBoolean(PROP_ANONYMIZE_STACKTRACES, true);
         s.putBoolean(PROP_ANONYMIZE_MESSAGES, false);

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/StandInStacktraceProvider.java
@@ -20,8 +20,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.recommenders.utils.Reflections;
 
-import com.google.common.collect.Sets;
-
 public class StandInStacktraceProvider {
 
     public static class StandInException extends RuntimeException {
@@ -35,22 +33,6 @@ public class StandInStacktraceProvider {
 
     private static final String STAND_IN_MESSAGE = "Stand-In Stacktrace supplied by Eclipse Stacktraces & Error Reporting Tool";
 
-    private static final Set<String> BLACKLISTED_CLASSNAMES = Sets.newHashSet();
-
-    static {
-        BLACKLISTED_CLASSNAMES.add("java.security.AccessController");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.Log");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.RuntimeLog");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.PlatformLogWriter");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogReaderServiceFactory");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogReaderServiceFactory$3");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogServiceFactory");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogServiceImpl");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.LoggerImpl");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.recommenders.internal.stacktraces.rcp.DebugStacktraceProvider");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.recommenders.internal.stacktraces.rcp.LogListener");
-    }
-
     private static Method SET_EXCEPTION = Reflections.getDeclaredMethod(Status.class, "setException", Throwable.class)
             .orNull();
 
@@ -59,7 +41,8 @@ public class StandInStacktraceProvider {
             Throwable syntetic = new StandInException(STAND_IN_MESSAGE);
             syntetic.fillInStackTrace();
             StackTraceElement[] stacktrace = syntetic.getStackTrace();
-            StackTraceElement[] clearedStacktrace = clearBlacklistedTopStackframes(stacktrace, BLACKLISTED_CLASSNAMES);
+            StackTraceElement[] clearedStacktrace = clearBlacklistedTopStackframes(stacktrace,
+                    Configuration.STAND_IN_STACKTRACE_BLACKLIST);
             syntetic.setStackTrace(clearedStacktrace);
             try {
                 SET_EXCEPTION.invoke(status, syntetic);

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/model/ErrorReports.java
@@ -25,6 +25,7 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.recommenders.internal.stacktraces.rcp.Configuration;
 import org.eclipse.recommenders.internal.stacktraces.rcp.model.impl.VisitorImpl;
 import org.eclipse.recommenders.utils.AnonymousId;
 import org.eclipse.recommenders.utils.gson.EmfFieldExclusionStrategy;
@@ -33,7 +34,6 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.hash.Hashing;
@@ -45,12 +45,12 @@ public class ErrorReports {
     public static final class ClearMessagesVisitor extends VisitorImpl {
         @Override
         public void visit(Status status) {
-            status.setMessage(HIDDEN);
+            status.setMessage(Configuration.HIDDEN);
         }
 
         @Override
         public void visit(Throwable throwable) {
-            throwable.setMessage(HIDDEN);
+            throwable.setMessage(Configuration.HIDDEN);
         }
     }
 
@@ -64,16 +64,16 @@ public class ErrorReports {
         @Override
         public void visit(Throwable throwable) {
             if (!isWhitelisted(throwable.getClassName(), whitelist)) {
-                throwable.setClassName(HIDDEN);
+                throwable.setClassName(Configuration.HIDDEN);
             }
         }
 
         @Override
         public void visit(StackTraceElement element) {
             if (!isWhitelisted(element.getClassName(), whitelist)) {
-                element.setClassName(HIDDEN);
-                element.setMethodName(HIDDEN);
-                element.setFileName(HIDDEN);
+                element.setClassName(Configuration.HIDDEN);
+                element.setMethodName(Configuration.HIDDEN);
+                element.setFileName(Configuration.HIDDEN);
                 element.setLineNumber(-1);
             }
         }
@@ -190,7 +190,7 @@ public class ErrorReports {
 
     }
 
-    private static class StatusFilterSetting {
+    public static class StatusFilterSetting {
 
         public StatusFilterSetting(String pluginPrefix, String... stacktraceClassPrefixes) {
             this.pluginPrefix = pluginPrefix;
@@ -201,6 +201,8 @@ public class ErrorReports {
         final List<String> stacktraceClassPrefixes;
     }
 
+    private static ModelFactory factory = ModelFactory.eINSTANCE;
+
     static boolean isWhitelisted(String className, List<String> whitelist) {
         for (String whiteListedPrefix : whitelist) {
             if (className.startsWith(whiteListedPrefix)) {
@@ -209,76 +211,6 @@ public class ErrorReports {
         }
         return false;
     }
-
-    private static final String HIDDEN = "HIDDEN";
-    private static ModelFactory factory = ModelFactory.eINSTANCE;
-    private static final String SOURCE_BEGIN_MESSAGE = "----------------------------------- SOURCE BEGIN -------------------------------------";
-    private static final String SOURCE_FILE_REMOVED = "source file contents removed";
-    private static final List<StatusFilterSetting> STATUS_FILTER_SETTINGS = ImmutableList
-            .of(
-            // at java.lang.Object.wait(Object.java:-2)
-            // at java.lang.Object.wait(Object.java:502)
-            // at org.eclipse.osgi.framework.eventmgr.EventManager$EventThread.getNextEvent(EventManager.java:400)
-            // at org.eclipse.osgi.framework.eventmgr.EventManager$EventThread.run(EventManager.java:336)
-            new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                    "org.eclipse.osgi.framework.eventmgr.EventManager",
-                    "org.eclipse.osgi.framework.eventmgr.EventManager"),
-            // at java.lang.Object.wait(Object.java:-2)
-            // at org.eclipse.core.internal.jobs.WorkerPool.sleep(WorkerPool.java:188)
-            // at org.eclipse.core.internal.jobs.WorkerPool.startJob(WorkerPool.java:220)
-            // at org.eclipse.core.internal.jobs.Worker.run(Worker.java:52)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.core.internal.jobs.WorkerPool", "org.eclipse.core.internal.jobs.WorkerPool",
-                            "org.eclipse.core.internal.jobs.Worker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at org.eclipse.jdt.internal.core.search.processing.JobManager.run(JobManager.java:382)
-                    // at java.lang.Thread.run(Thread.java:745)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "org.eclipse.jdt.internal.core.search.processing.JobManager", "java.lang.Thread"),
-                    // at org.eclipse.pde.internal.core.PluginModelManager.initializeTable(PluginModelManager.java:496)
-                    // at org.eclipse.pde.internal.core.PluginModelManager.targetReloaded(PluginModelManager.java:473)
-                    // at
-                    // org.eclipse.pde.internal.core.RequiredPluginsInitializer$1.run(RequiredPluginsInitializer.java:34)
-                    // at org.eclipse.core.internal.jobs.Worker.run(Worker.java:55)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring",
-                            "org.eclipse.pde.internal.core.PluginModelManager",
-                            "org.eclipse.pde.internal.core.PluginModelManager",
-                            "org.eclipse.pde.internal.core.RequiredPluginsInitializer",
-                            "org.eclipse.core.internal.jobs.Worker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:142)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:158)
-                    // at
-                    // org.eclipse.emf.common.util.CommonUtil$1ReferenceClearingQueuePollingThread.run(CommonUtil.java:70)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "java.lang.ref.ReferenceQueue", "java.lang.ref.ReferenceQueue",
-                            "org.eclipse.emf.common.util.CommonUtil"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at org.eclipse.core.internal.jobs.InternalWorker.run(InternalWorker.java:59)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.core.internal.jobs.InternalWorker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at org.eclipse.equinox.internal.util.impl.tpt.timer.TimerImpl.run(TimerImpl.java:141)
-                    // at java.lang.Thread.run(Thread.java:745)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.equinox.internal.util.impl.tpt.timer.TimerImpl", "java.lang.Thread"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:142)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:158)
-                    // at java.lang.ref.Finalizer$FinalizerThread.run(Finalizer.java:209)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "java.lang.ref.ReferenceQueue", "java.lang.ref.ReferenceQueue", "java.lang.ref.Finalizer"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at java.lang.ref.Reference$ReferenceHandler.run(Reference.java:157)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "java.lang.ref.Reference$ReferenceHandler"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at org.eclipse.equinox.internal.util.impl.tpt.threadpool.Executor.run(Executor.java:106)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "org.eclipse.equinox.internal.util.impl.tpt.threadpool.Executor"));
 
     public static ErrorReport copy(ErrorReport org) {
         return EcoreUtil.copy(org);
@@ -399,7 +331,7 @@ public class ErrorReports {
         if (status.getCode() != 0 && status.getCode() != 1) {
             return false;
         }
-        for (StatusFilterSetting filterSetting : STATUS_FILTER_SETTINGS) {
+        for (StatusFilterSetting filterSetting : Configuration.MULTISTATUS_CHILD_STACKTRACES_FILTER_SETTINGS) {
             if (status.getPlugin().startsWith(filterSetting.pluginPrefix)
                     && isStacktraceFiltered(status, filterSetting.stacktraceClassPrefixes)) {
                 return true;
@@ -422,8 +354,8 @@ public class ErrorReports {
     }
 
     private static String removeSourceFileContents(String message) {
-        if (message.contains(SOURCE_BEGIN_MESSAGE)) {
-            return SOURCE_FILE_REMOVED;
+        if (message.contains(Configuration.SOURCE_BEGIN_MESSAGE)) {
+            return Configuration.SOURCE_FILE_REMOVED;
         } else {
             return message;
         }

kolipakakondal/error_reporting

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/PreferenceInitializer.java
@@ -40,9 +40,8 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
         s.put(PROP_NAME, "");
         s.put(PROP_EMAIL, "");
         s.putBoolean(PROP_SKIP_SIMILAR_ERRORS, true);
-        s.put(PROP_WHITELISTED_PLUGINS, "org.eclipse.;org.apache.log4j.;com.codetrails.;");
-        s.put(PROP_WHITELISTED_PACKAGES,
-                "org.eclipse.;;;org.apache.;java.;javax.;javafx.;sun.;com.sun.;com.codetrails.;org.osgi.;com.google.;ch.qos.;org.slf4j.;");
+        s.put(PROP_WHITELISTED_PLUGINS, Constants.WHITELISTED_PLUGINS);
+        s.put(PROP_WHITELISTED_PACKAGES, Constants.WHITELISTED_PACKAGES);
         s.put(PROP_SEND_ACTION, SendAction.ASK.name());
         s.putBoolean(PROP_ANONYMIZE_STACKTRACES, true);
         s.putBoolean(PROP_ANONYMIZE_MESSAGES, false);

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/StandInStacktraceProvider.java
@@ -20,8 +20,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.recommenders.utils.Reflections;
 
-import com.google.common.collect.Sets;
-
 public class StandInStacktraceProvider {
 
     public static class StandInException extends RuntimeException {
@@ -35,22 +33,6 @@ public class StandInStacktraceProvider {
 
     private static final String STAND_IN_MESSAGE = "Stand-In Stacktrace supplied by Eclipse Stacktraces & Error Reporting Tool";
 
-    private static final Set<String> BLACKLISTED_CLASSNAMES = Sets.newHashSet();
-
-    static {
-        BLACKLISTED_CLASSNAMES.add("java.security.AccessController");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.Log");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.RuntimeLog");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.core.internal.runtime.PlatformLogWriter");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogReaderServiceFactory");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogReaderServiceFactory$3");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogServiceFactory");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.ExtendedLogServiceImpl");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.osgi.internal.log.LoggerImpl");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.recommenders.internal.stacktraces.rcp.StandInStacktraceProvider");
-        BLACKLISTED_CLASSNAMES.add("org.eclipse.recommenders.internal.stacktraces.rcp.LogListener");
-    }
-
     private static Method SET_EXCEPTION = Reflections.getDeclaredMethod(Status.class, "setException", Throwable.class)
             .orNull();
 
@@ -59,7 +41,8 @@ public class StandInStacktraceProvider {
             Throwable syntetic = new StandInException(STAND_IN_MESSAGE);
             syntetic.fillInStackTrace();
             StackTraceElement[] stacktrace = syntetic.getStackTrace();
-            StackTraceElement[] clearedStacktrace = clearBlacklistedTopStackframes(stacktrace, BLACKLISTED_CLASSNAMES);
+            StackTraceElement[] clearedStacktrace = clearBlacklistedTopStackframes(stacktrace,
+                    Constants.STAND_IN_STACKTRACE_BLACKLIST);
             syntetic.setStackTrace(clearedStacktrace);
             try {
                 SET_EXCEPTION.invoke(status, syntetic);

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/model/ErrorReports.java
@@ -25,15 +25,14 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.recommenders.internal.stacktraces.rcp.Constants;
 import org.eclipse.recommenders.internal.stacktraces.rcp.model.impl.VisitorImpl;
 import org.eclipse.recommenders.utils.AnonymousId;
 import org.eclipse.recommenders.utils.gson.EmfFieldExclusionStrategy;
 import org.eclipse.recommenders.utils.gson.UuidTypeAdapter;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.Constants;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.hash.Hashing;
@@ -45,12 +44,12 @@ public class ErrorReports {
     public static final class ClearMessagesVisitor extends VisitorImpl {
         @Override
         public void visit(Status status) {
-            status.setMessage(HIDDEN);
+            status.setMessage(Constants.HIDDEN);
         }
 
         @Override
         public void visit(Throwable throwable) {
-            throwable.setMessage(HIDDEN);
+            throwable.setMessage(Constants.HIDDEN);
         }
     }
 
@@ -64,16 +63,16 @@ public class ErrorReports {
         @Override
         public void visit(Throwable throwable) {
             if (!isWhitelisted(throwable.getClassName(), whitelist)) {
-                throwable.setClassName(HIDDEN);
+                throwable.setClassName(Constants.HIDDEN);
             }
         }
 
         @Override
         public void visit(StackTraceElement element) {
             if (!isWhitelisted(element.getClassName(), whitelist)) {
-                element.setClassName(HIDDEN);
-                element.setMethodName(HIDDEN);
-                element.setFileName(HIDDEN);
+                element.setClassName(Constants.HIDDEN);
+                element.setMethodName(Constants.HIDDEN);
+                element.setFileName(Constants.HIDDEN);
                 element.setLineNumber(-1);
             }
         }
@@ -190,7 +189,7 @@ public class ErrorReports {
 
     }
 
-    private static class StatusFilterSetting {
+    public static class StatusFilterSetting {
 
         public StatusFilterSetting(String pluginPrefix, String... stacktraceClassPrefixes) {
             this.pluginPrefix = pluginPrefix;
@@ -201,6 +200,8 @@ public class ErrorReports {
         final List<String> stacktraceClassPrefixes;
     }
 
+    private static ModelFactory factory = ModelFactory.eINSTANCE;
+
     static boolean isWhitelisted(String className, List<String> whitelist) {
         for (String whiteListedPrefix : whitelist) {
             if (className.startsWith(whiteListedPrefix)) {
@@ -209,76 +210,6 @@ public class ErrorReports {
         }
         return false;
     }
-
-    private static final String HIDDEN = "HIDDEN";
-    private static ModelFactory factory = ModelFactory.eINSTANCE;
-    private static final String SOURCE_BEGIN_MESSAGE = "----------------------------------- SOURCE BEGIN -------------------------------------";
-    private static final String SOURCE_FILE_REMOVED = "source file contents removed";
-    private static final List<StatusFilterSetting> STATUS_FILTER_SETTINGS = ImmutableList
-            .of(
-            // at java.lang.Object.wait(Object.java:-2)
-            // at java.lang.Object.wait(Object.java:502)
-            // at org.eclipse.osgi.framework.eventmgr.EventManager$EventThread.getNextEvent(EventManager.java:400)
-            // at org.eclipse.osgi.framework.eventmgr.EventManager$EventThread.run(EventManager.java:336)
-            new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                    "org.eclipse.osgi.framework.eventmgr.EventManager",
-                    "org.eclipse.osgi.framework.eventmgr.EventManager"),
-            // at java.lang.Object.wait(Object.java:-2)
-            // at org.eclipse.core.internal.jobs.WorkerPool.sleep(WorkerPool.java:188)
-            // at org.eclipse.core.internal.jobs.WorkerPool.startJob(WorkerPool.java:220)
-            // at org.eclipse.core.internal.jobs.Worker.run(Worker.java:52)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.core.internal.jobs.WorkerPool", "org.eclipse.core.internal.jobs.WorkerPool",
-                            "org.eclipse.core.internal.jobs.Worker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at org.eclipse.jdt.internal.core.search.processing.JobManager.run(JobManager.java:382)
-                    // at java.lang.Thread.run(Thread.java:745)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "org.eclipse.jdt.internal.core.search.processing.JobManager", "java.lang.Thread"),
-                    // at org.eclipse.pde.internal.core.PluginModelManager.initializeTable(PluginModelManager.java:496)
-                    // at org.eclipse.pde.internal.core.PluginModelManager.targetReloaded(PluginModelManager.java:473)
-                    // at
-                    // org.eclipse.pde.internal.core.RequiredPluginsInitializer$1.run(RequiredPluginsInitializer.java:34)
-                    // at org.eclipse.core.internal.jobs.Worker.run(Worker.java:55)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring",
-                            "org.eclipse.pde.internal.core.PluginModelManager",
-                            "org.eclipse.pde.internal.core.PluginModelManager",
-                            "org.eclipse.pde.internal.core.RequiredPluginsInitializer",
-                            "org.eclipse.core.internal.jobs.Worker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:142)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:158)
-                    // at
-                    // org.eclipse.emf.common.util.CommonUtil$1ReferenceClearingQueuePollingThread.run(CommonUtil.java:70)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "java.lang.ref.ReferenceQueue", "java.lang.ref.ReferenceQueue",
-                            "org.eclipse.emf.common.util.CommonUtil"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at org.eclipse.core.internal.jobs.InternalWorker.run(InternalWorker.java:59)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.core.internal.jobs.InternalWorker"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at org.eclipse.equinox.internal.util.impl.tpt.timer.TimerImpl.run(TimerImpl.java:141)
-                    // at java.lang.Thread.run(Thread.java:745)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "org.eclipse.equinox.internal.util.impl.tpt.timer.TimerImpl", "java.lang.Thread"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:142)
-                    // at java.lang.ref.ReferenceQueue.remove(ReferenceQueue.java:158)
-                    // at java.lang.ref.Finalizer$FinalizerThread.run(Finalizer.java:209)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object",
-                            "java.lang.ref.ReferenceQueue", "java.lang.ref.ReferenceQueue", "java.lang.ref.Finalizer"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at java.lang.ref.Reference$ReferenceHandler.run(Reference.java:157)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "java.lang.ref.Reference$ReferenceHandler"),
-                    // at java.lang.Object.wait(Object.java:-2)
-                    // at java.lang.Object.wait(Object.java:502)
-                    // at org.eclipse.equinox.internal.util.impl.tpt.threadpool.Executor.run(Executor.java:106)
-                    new StatusFilterSetting("org.eclipse.ui.monitoring", "java.lang.Object", "java.lang.Object",
-                            "org.eclipse.equinox.internal.util.impl.tpt.threadpool.Executor"));
 
     public static ErrorReport copy(ErrorReport org) {
         return EcoreUtil.copy(org);
@@ -325,8 +256,8 @@ public class ErrorReports {
         mReport.setEclipseProduct(System.getProperty("eclipse.product", "-"));
         mReport.setOsgiArch(System.getProperty("osgi.arch", "-"));
         mReport.setOsgiWs(System.getProperty("osgi.ws", "-"));
-        mReport.setOsgiOs(System.getProperty(Constants.FRAMEWORK_OS_NAME, "-"));
-        mReport.setOsgiOsVersion(System.getProperty(Constants.FRAMEWORK_OS_VERSION, "-"));
+        mReport.setOsgiOs(System.getProperty(org.osgi.framework.Constants.FRAMEWORK_OS_NAME, "-"));
+        mReport.setOsgiOsVersion(System.getProperty(org.osgi.framework.Constants.FRAMEWORK_OS_VERSION, "-"));
         mReport.setStatus(newStatus(event, settings));
 
         guessInvolvedPlugins(mReport);
@@ -399,7 +330,7 @@ public class ErrorReports {
     }
 
     private static boolean isFiltered(IStatus status) {
-        for (StatusFilterSetting filterSetting : STATUS_FILTER_SETTINGS) {
+        for (StatusFilterSetting filterSetting : Constants.MULTISTATUS_CHILD_STACKTRACES_FILTER_SETTINGS) {
             if (status.getPlugin().startsWith(filterSetting.pluginPrefix)
                     && isStacktraceFiltered(status, filterSetting.stacktraceClassPrefixes)) {
                 return true;
@@ -422,8 +353,8 @@ public class ErrorReports {
     }
 
     private static String removeSourceFileContents(String message) {
-        if (message.contains(SOURCE_BEGIN_MESSAGE)) {
-            return SOURCE_FILE_REMOVED;
+        if (message.contains(Constants.SOURCE_BEGIN_MESSAGE)) {
+            return Constants.SOURCE_FILE_REMOVED;
         } else {
             return message;
         }