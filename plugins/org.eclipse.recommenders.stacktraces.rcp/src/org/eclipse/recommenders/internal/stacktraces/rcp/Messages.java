eclipse/recommenders

[stacktraces] Bug 445636: Small message updates
Change-Id: I319a7da44716a585909599badd49d9b06ba2f134
Signed-off-by: Daniel Haftstein <haftsteind@gmail.com>

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/Messages.java
@@ -35,6 +35,7 @@ public class Messages extends NLS {
     public static String SETTINGSPAGE_GROUPLABEL_PERSONAL;
     public static String LINK_LEARN_MORE;
     public static String LINK_PROVIDE_FEEDBACK;
+    public static String PREFERENCE_PAGE_ASK_LABEL;
     public static String PREVIEWPAGE_DESC;
     public static String PREVIEWPAGE_LABEL_COMMENT;
     public static String PREVIEWPAGE_LABEL_MESSAGE;

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/PreferencePage.java
@@ -49,7 +49,6 @@ public class PreferencePage extends FieldEditorPreferencePage implements IWorkbe
                 getFieldEditorParent()));
         addField(new BooleanFieldEditor(PROP_ANONYMIZE_STACKTRACES, Messages.FIELD_LABEL_ANONYMIZE_STACKTRACES,
                 getFieldEditorParent()));
-
         addField(new BooleanFieldEditor(PROP_ANONYMIZE_MESSAGES, Messages.FIELD_LABEL_ANONYMIZE_MESSAGES,
                 getFieldEditorParent()));
         addField(new BooleanFieldEditor(PROP_SKIP_SIMILAR_ERRORS, Messages.FIELD_LABEL_SKIP_SIMILAR_ERRORS,
@@ -93,7 +92,7 @@ public class PreferencePage extends FieldEditorPreferencePage implements IWorkbe
     private static String descriptionForMode(SendAction mode) {
         switch (mode) {
         case ASK:
-            return Messages.FIELD_LABEL_ACTION_REPORT_ASK;
+            return Messages.PREFERENCE_PAGE_ASK_LABEL;
         case IGNORE:
             return Messages.FIELD_LABEL_ACTION_REPORT_NEVER;
         case SILENT:

kolipakakondal/error_reporting

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/Messages.java
@@ -36,6 +36,7 @@ public class Messages extends NLS {
     public static String SETTINGSPAGE_GROUPLABEL_PERSONAL;
     public static String LINK_LEARN_MORE;
     public static String LINK_PROVIDE_FEEDBACK;
+    public static String PREFERENCEPAGE_ASK_LABEL;
     public static String PREVIEWPAGE_DESC;
     public static String PREVIEWPAGE_LABEL_COMMENT;
     public static String PREVIEWPAGE_LABEL_MESSAGE;

plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/PreferencePage.java
@@ -109,7 +109,7 @@ public class PreferencePage extends FieldEditorPreferencePage implements IWorkbe
     private static String descriptionForMode(SendAction mode) {
         switch (mode) {
         case ASK:
-            return Messages.FIELD_LABEL_ACTION_REPORT_ASK;
+            return Messages.PREFERENCEPAGE_ASK_LABEL;
         case IGNORE:
             return Messages.FIELD_LABEL_ACTION_REPORT_NEVER;
         case SILENT: